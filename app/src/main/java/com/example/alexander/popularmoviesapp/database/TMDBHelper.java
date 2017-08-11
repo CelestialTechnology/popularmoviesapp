470858703667425ec6ee9295999fea0ec63367c0
CelestialTechnology <awestbrooks50@gmail.com>
2017-08-11 02:39:58 -0500

    Implement Database For Application
    The create sql statement as well as the functionality for upgrading the database is implemented.

--- app/src/main/java/com/example/alexander/popularmoviesapp/database/TMDBHelper.java
+++ app/src/main/java/com/example/alexander/popularmoviesapp/database/TMDBHelper.java
@@ -10,18 +10,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class TMDBHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "tdmbfavorites.db";
+    private static final int DATABASE_VERSION = 1;
+
     public TMDBHelper(Context context) {
-        // TODO: FILL In the proper credentials for this project
-        super(context, "", null, 0);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createTableStatement = "CREATE TABLE " +
+                TMDBContract.FavoriteMovieEntry.TABLE_NAME + "(" +
+                TMDBContract.FavoriteMovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_TITLE + " TEXT NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_OVERVIEW + " TEXT NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_VOTE_AVERAGE + " REAL NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_RELEASE_DATE + " TEXT NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_POSTER + " BLOB NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_POSTER_PATH + " TEXT NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_BACKDROP + " BLOB NOT NULL," +
+                TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_BACKDROP_PATH + " TEXT NOT NULL" +
+                ");";
+        db.execSQL(createTableStatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TMDBContract.FavoriteMovieEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
