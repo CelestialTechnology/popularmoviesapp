b8c1cfeb94f6aea34e0301598f748cafb52810dd
CelestialTechnology <awestbrooks50@gmail.com>
2017-08-11 01:42:33 -0500

    Create Review Class
    This class will be the container for each of the reviews related to the movie loaded in the DetailActivityFragment.

---
+++ app/src/main/java/com/example/alexander/popularmoviesapp/Review.java
@@ -0,0 +1,23 @@
+package com.example.alexander.popularmoviesapp;
+
+/**
+ * Created by awest on 8/11/2017.
+ */
+
+public class Review {
+    private String author;
+    private String content;
+
+    public Review(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
