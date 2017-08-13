714fd7de1764ff7bf54665079d917074fd1acbc3
CelestialTechnology <awestbrooks50@gmail.com>
2017-08-13 16:22:32 -0500

    Create Urimatcher for Content Provider
    Set up uri matcher with the two types of Uris the application requires and store with
    corresponding Uri codes.

--- app/src/main/java/com/example/alexander/popularmoviesapp/database/TMDBProvider.java
+++ app/src/main/java/com/example/alexander/popularmoviesapp/database/TMDBProvider.java
@@ -3,6 +3,7 @@ package com.example.alexander.popularmoviesapp.database;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -16,10 +17,30 @@ public class TMDBProvider extends ContentProvider {
 
     private TMDBHelper tmdbHelper;
 
-    // Necessary Uris:
-    // Get all stored movies
-    // Get One stored movie
+    // Codes for the different Uri types
+    public static final int FAVORITE_MOVIES = 100;
+    public static final int FAVORITE_MOVIE_WITH_ID = 101;
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    // Uri matcher
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        // Add the needed uris to the matcher
+        // Uri for all database contents
+        uriMatcher.addURI(
+                TMDBContract.CONTENT_AUTHORITY,
+                TMDBContract.PATH_FAVORITE_MOVIES,
+                FAVORITE_MOVIES);
+        // Uri for a single stored movie
+        uriMatcher.addURI(
+                TMDBContract.CONTENT_AUTHORITY,
+                TMDBContract.PATH_FAVORITE_MOVIES + "/#",
+                FAVORITE_MOVIE_WITH_ID);
+
+        return uriMatcher;
+    }
     @Override
     public boolean onCreate() {
         Context context = getContext();
