cddcbc01f16cedce371f928d4b074233d39cd01d
CelestialTechnology <awestbrooks50@gmail.com>
2017-08-14 12:04:50 -0500

    Implement the LoadFavoriteMoviesTask
    Does the background work to retrieve movies stored in the database.

--- app/src/main/java/com/example/alexander/popularmoviesapp/jsondata/LoadFavoriteMoviesTask.java
+++ app/src/main/java/com/example/alexander/popularmoviesapp/jsondata/LoadFavoriteMoviesTask.java
@@ -1,30 +1,96 @@
 package com.example.alexander.popularmoviesapp.jsondata;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.os.AsyncTask;
 import android.widget.ArrayAdapter;
 
+import com.example.alexander.popularmoviesapp.moviedata.DownloadedMovie;
 import com.example.alexander.popularmoviesapp.moviedata.Movie;
+
+import java.util.ArrayList;
+
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_ID;
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_OVERVIEW;
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_RELEASE_DATE;
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_TITLE;
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.COLUMN_MOVIE_VOTE_AVERAGE;
+import static com.example.alexander.popularmoviesapp.database.TMDBContract.FavoriteMovieEntry.CONTENT_URI;
+
 
 /**
  * Created by awest on 8/13/2017.
  */
 
-public class LoadFavoriteMoviesTask extends AsyncTask<Void, Void, Cursor> {
+public class LoadFavoriteMoviesTask extends AsyncTask<Void, Void, ArrayList<Movie>> {
 
     private ArrayAdapter<Movie> mAdapter;
+    private Context mContext;
 
-    public LoadFavoriteMoviesTask(ArrayAdapter<Movie> adapter) {
+    public LoadFavoriteMoviesTask(ArrayAdapter<Movie> adapter, Context context) {
         mAdapter = adapter;
+        mContext = context;
     }
 
     @Override
-    protected Cursor doInBackground(Void... params) {
+    protected ArrayList<Movie> doInBackground(Void... params) {
+        try {
+            ArrayList<Movie> movies = new ArrayList<>();
+            String[] projection = new String[]{
+                    COLUMN_MOVIE_ID,
+                    COLUMN_MOVIE_TITLE,
+                    COLUMN_MOVIE_OVERVIEW,
+                    COLUMN_MOVIE_VOTE_AVERAGE,
+                    COLUMN_MOVIE_RELEASE_DATE
+            };
+            Cursor cursor = mContext.getContentResolver().query(
+                    CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null
+            );
+
+            if (cursor != null && cursor.getCount() > 0) {
+
+                while (cursor.moveToNext()) {
+                    int idIndex = cursor.getColumnIndex(COLUMN_MOVIE_ID);
+                    int titleIndex = cursor.getColumnIndex(COLUMN_MOVIE_TITLE);
+                    int overviewIndex = cursor.getColumnIndex(COLUMN_MOVIE_OVERVIEW);
+                    int voteAvgIndex = cursor.getColumnIndex(COLUMN_MOVIE_VOTE_AVERAGE);
+                    int releaseDateIndex = cursor.getColumnIndex(COLUMN_MOVIE_RELEASE_DATE);
+
+                    String id = cursor.getString(idIndex);
+                    String title = cursor.getString(titleIndex);
+                    String overview = cursor.getString(overviewIndex);
+                    double voteAvg = cursor.getDouble(voteAvgIndex);
+                    String releaseDate = cursor.getString(releaseDateIndex);
+
+                    movies.add(new DownloadedMovie(
+                            id,
+                            title,
+                            overview,
+                            voteAvg,
+                            releaseDate
+                    ));
+
+                }
+                return movies;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    protected void onPostExecute(Cursor cursor) {
-        super.onPostExecute(cursor);
+    protected void onPostExecute(ArrayList<Movie> movies) {
+        super.onPostExecute(movies);
+        if (movies != null && movies.size() > 0) {
+            for (Movie m : movies) {
+                mAdapter.add(m);
+            }
+        }
     }
 }
