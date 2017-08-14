e392c1bc3df2994d28dc41b1e6f99cf5ae08e4a3
CelestialTechnology <awestbrooks50@gmail.com>
2017-08-14 16:12:39 -0500

    Implement Trailer Adapter to create views for trailers

---
+++ app/src/main/java/com/example/alexander/popularmoviesapp/TrailerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.alexander.popularmoviesapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.alexander.popularmoviesapp.moviedata.Trailer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by awest on 8/14/2017.
+ */
+
+public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TrailerViewHolder> {
+    private ArrayList<Trailer> trailers;
+
+    public TrailerAdapter(ArrayList<Trailer> trailers) {
+        this.trailers = trailers;
+    }
+
+    @Override
+    public TrailerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        int layoutItemId = R.layout.trailer_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutItemId, parent, shouldAttachToParentImmediately);
+        TrailerViewHolder trailerViewHolder = new TrailerViewHolder(view);
+
+        return trailerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TrailerViewHolder holder, int position) {
+        holder.bind(trailers.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class TrailerViewHolder extends RecyclerView.ViewHolder {
+        private TextView trailerName;
+
+        public TrailerViewHolder(View itemView) {
+            super(itemView);
+            trailerName = (TextView) itemView.findViewById(R.id.textViewName);
+        }
+
+        void bind(Trailer trailer) {
+            trailerName.setText(trailer.getName());
+        }
+    }
+}
